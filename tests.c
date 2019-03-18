08d2e37ca2df9af50416abdccb10ab29a74735f8
dalitroz <dalitrozin@gmail.com>
2019-03-18 08:57:30 +0200

    made test nicer

--- tests.c
+++ tests.c
@@ -3,86 +3,68 @@
 #include "user.h"
 
 
-
-
 //int test_exit(){}
 
 //int test_wait(){}
 
-
-void test_detach_success(int * ans){
+int success=0, fail=0,ans=1;
+void test_detach_success(){
   int pid;
   pid = fork();
   if(pid > 0) {
+   ans = detach(pid);
 
-   *ans = detach(pid);
-
-   // have a child with this pid.
 } else
   {
     sleep(10);
     exit(0);
   }
-
-
 }
-void test_detach_fail(int * ans){
+
+void test_detach_fail(){
   int pid;
   pid = fork();
   if(pid > 0) {
     wait(0);
-   *ans = detach(pid+9); // status = -1
+   ans = detach(pid+9); // status = -1
 
-   // have a child with this pid.
   }
   else
-  { //sleep(10);
+  {
     exit(0);
   }
 
 }
 
-void test_detach_wrong_parent(int * ans){
+void test_detach_wrong_parent(){
   int pid;
   pid = fork();
   if(pid == 0) {
-      *ans = detach(pid); // status = -1
+      ans = detach(pid); // status = -1
       exit(0);
   }
   else wait(0);
 }
 
+void make_test(void (*f)(void) , int expected ,char * fail_msg){
 
+  (*f)();
+  if(ans == expected)
+    success++;
+  else {
+    fail++;
+    printf(2,"%s\n",fail_msg);
+  }
+
+}
 
 int main(void){
-  static int success=0, fail=0;
-  int * ans = malloc(sizeof(int));
-  *ans = 1;
-  test_detach_success(ans);
-  if(*ans!=0){//here we want 0;
-    fail++;
-    printf(2,"test_detach_success failed\n");
-  }
-   else success++;
 
-   test_detach_fail(ans);
-   if(*ans!=-1){
-     fail++;
-     printf(2,"test_detach_fail failed\n");
-   }
-   else success++;
+   make_test(test_detach_success,0,"test_detach_success failed\n");
+   make_test(test_detach_fail,-1,"test_detach_fail failed\n");
+   make_test(test_detach_wrong_parent,-1,"test_detach_wrong_parent failed\n");
+   printf(2,"num of success:%d num of failures: %d\n",success,fail );
 
-    test_detach_wrong_parent(ans);
-    if(*ans!=-1){
-      fail++;
-      printf(2,"test_detach_wrong_parent failed\n");
-    }
-    else
-      success++;
-
-    printf(2,"num of success:%d num of failures: %d\n",success,fail );
-    exit(0);
-
-
+   exit(0);
 
 }
